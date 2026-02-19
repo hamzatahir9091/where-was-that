dcc762511cb72f053b24b936e5ce4d4c00cc09e6
Hamza Tahir <hamzatahir9091@gmail.com>
2026-02-19 11:06:33 +0500

    first working prototype

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
-function addPin() {
-	const MARKERS = new Map()
+const MARKERS = new Map()
 
+function addPin() {
 	if (document.getElementById("pinPopup")) return
 
 	const pop = document.createElement("div")
@@ -50,8 +50,84 @@ function addPin() {
 }
 
 function savePin() {
-    const title = document.getElementById("pinTitle")
-    const notes = document.getElementById("pinNotes")
+	const title = document.getElementById("pinTitle").value
+	const notes = document.getElementById("pinNotes").value
+	const scroll = window.scrollY
 
-    
+	const pin = {
+		title,
+		notes,
+		scrollOffset: scroll,
+	}
+
+	// getting the url
+	const url = window.location.href
+
+	if (MARKERS.has(url)) {
+		// if site has already pins
+		MARKERS.get(url).push(pin)
+	} else {
+		MARKERS.set(url, [pin])
+	}
+
+	// Convert Map to plain object and save
+	chrome.storage.local.set(Object.fromEntries(MARKERS), () => {
+		console.log("Pins saved to Chrome storage!")
+	})
+
+	// close popup after saving
+	document.getElementById("pinPopup")?.remove()
 }
+
+function loadPins() {
+	if (document.getElementById("savedPins")) {
+		document.getElementById("savedPins").remove
+	}
+
+	const url = window.location.href
+
+	chrome.storage.local.get(null, (result) => {
+		MARKERS.clear()
+		for (const [url, pins] of Object.entries(result)) {
+			MARKERS.set(url, pins)
+		}
+
+		const currentPins = MARKERS.get(url) || [] // return empty array if no pins
+
+		let savedPins = document.createElement("div")
+		savedPins.id = "savedPins"
+
+		for (const pin of currentPins) {
+			savedPins.innerHTML += `
+            <div class="writtenTitle">
+            ${pin.title}
+            </div>`
+		}
+
+		Object.assign(savedPins.style, {
+			width: "250px",
+			padding: "12px",
+			background: "rgba(0,0,0,0.7)",
+			backdropFilter: "blur(8px)",
+			color: "white",
+			position: "fixed",
+			top: "20px",
+			right: "20px",
+			borderRadius: "8px",
+			boxShadow: "0 4px 12px rgba(0,0,0,0.3)",
+			display: "flex",
+			flexDirection: "column",
+			gap: "8px",
+			zIndex: 999999,
+		})
+
+		document.body.appendChild(savedPins)
+	})
+}
+
+document.addEventListener("keydown", (e) => {
+	if (e.key === "S") addPin()
+	if (e.key === "O") loadPins()
+	if (e.key === "Escape") document.getElementById("pinPopup")?.remove()
+})
+
